e08e55d24af3bdb82f9db7bb45216513ca928bf0
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-12 12:27:00 +0100

    aggiunta delle proprieties

---
+++ Salone/src/main/java/com/team1/Salone/SaloneApplication.java
@@ -0,0 +1,13 @@
+package com.team1.Salone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaloneApplication.class, args);
+	}
+
+}
---
+++ Salone/src/test/java/com/team1/Salone/SaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.Salone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
